2fb7b380059fadef65725834bed7c4e20bfb0f2d
Jeremi <jrynk@hotmail.com>
2022-10-28 22:06:35 +0200

    fix LF vs CRLF

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-export JRPC_iotex_wallet_builder=$PWD
+#!/bin/bash
+export COMPOSE_DOCKER_CLI_BUILD=1
+export DOCKER_BUILDKIT=1
+
 docker-compose -f build/docker-compose.yml up --build --no-deps --remove-orphans
