933a57af318090cdcf38419ccaa92ea0a06d3a4a
Jeremi Rynkiewicz <jrynk@hotmail.com>
2022-11-06 09:59:21 -0800

    fixing compatibility with Alpine images

--- install.sh
+++ install.sh
@@ -2,4 +2,18 @@
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
 
-docker-compose -f build/docker-compose.yml up --build --no-deps --remove-orphans
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+# Start of script - getting necessary user input
+read -p "Do you want to build Alpine (${red}A${reset}) or Ubuntu (${green}U${reset}) AppImage/Snap binaries?: " MODE
+MODE_flag=$(case "$MODE" in
+  (A|a)    echo "alpine"  ;;
+  (U|u)    echo "ubuntu"  ;;
+  ( * )    echo "Error. Wrong Selection"  ;;
+esac)
+
+docker-compose -f build/docker-compose.yml build iotex-explorer-builder-$MODE_flag
+
+docker-compose -f build/docker-compose.yml up iotex-explorer-builder-$MODE_flag --remove-orphans
